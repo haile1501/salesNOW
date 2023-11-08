018131b95fafafbe46bde73ba46a8eda8b615fa6
Hải Lê <hai.lehoang1501@gmail.com>
2023-11-08 16:16:18 +0700

    init codebase

---
+++ salesnow-be/src/main/java/com/hust/salesnowbe/SalesnowBeApplication.java
@@ -0,0 +1,13 @@
+package com.hust.salesnowbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesnowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesnowBeApplication.class, args);
+	}
+
+}
