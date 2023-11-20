a156cce3daae4d61584f5779e72a05d9d5cc9e40
longgu02 <phamlong121120022@gmail.com>
2023-11-20 22:03:11 +0700

    feat: layout, header, sidebar

--- salesnow-fe/src/App.jsx
+++ salesnow-fe/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { AuthContext } from "./contexts/AuthContex";
+
+import { publicRoutes, privateRoutes } from "./routes";
+import DefaultLayout from "./layouts/DefaultLayout/DefaultLayout";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const { token } = useContext(AuthContext);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="App">
+			{/* {console.log('app user login', user)} */}
+			<Routes>
+				{publicRoutes.map((route, index) => {
+					const Page = route.component;
+					let Layout = DefaultLayout;
+					if (route.layout) {
+						Layout = route.layout;
+					} else if (route.layout === null) {
+						Layout = Fragment;
+					}
+
+					return (
+						<Route
+							key={index}
+							path={route.path}
+							element={
+								<Layout
+									title={route.title}
+									link={route.link}
+									btn_title={route.btn_title}
+								>
+									<Page />
+								</Layout>
+							}
+						/>
+					);
+				})}
+
+				{/* privateRoutes */}
+				{token && // Kiểm tra user có tồn tại hay không
+					privateRoutes.map((route, index) => {
+						const Page = route.component;
+						let Layout = DefaultLayout;
+						if (route.layout) {
+							Layout = route.layout;
+						} else if (route.layout === null) {
+							Layout = Fragment;
+						}
+
+						return (
+							<Route
+								key={index}
+								path={route.path}
+								element={
+									<Layout title={route.title}>
+										<Page />
+									</Layout>
+								}
+							/>
+						);
+					})}
+
+				{/* Điều hướng mặc định */}
+				{/* <Route path="*" element={<Navigate to="/login" />} /> */}
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ salesnow-fe/src/pages/Home/Home.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import classNames from "classnames/bind";
+import styles from "./Home.module.scss";
+import { useContext, useEffect, useState } from "react";
+import React from "react";
+// import { DataGrid } from "@mui/x-data-grid";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+// 	faBox,
+// 	faFileInvoiceDollar,
+// 	faSackDollar,
+// 	faUser,
+// } from "@fortawesome/free-solid-svg-icons";
+// import { fontSize } from "@mui/system";
+// import dayjs from "dayjs";
+// import Select from "react-select";
+// import numeral from "numeral";
+
+// import { AuthContext } from "../../contexts/AuthContex";
+// import * as OrderServices from "../../services/orderServices";
+// import * as CustomerServices from "../../services/CustomerServices";
+// import moment from "moment";
+import { Box } from "@mui/material";
+import DefaultLayout from "../../layouts/DefaultLayout/DefaultLayout";
+
+const cx = classNames.bind(styles);
+
+const customStyles = {
+	option: (provided, state) => ({
+		...provided,
+
+		cursor: "pointer",
+	}),
+
+	control: (provided) => ({
+		...provided,
+		minHeight: "40px",
+		width: "180px",
+	}),
+};
+
+function Dashboard() {
+	console.log("hehe");
+	return <DefaultLayout>home</DefaultLayout>;
+}
+
+export default Dashboard;
