e49172a2f113582553469ab6bafff55b6278a349
longgu02 <phamlong121120022@gmail.com>
2023-11-25 16:31:57 +0700

    feat: config routing

--- salesnow-fe/src/routes/index.js
+++ salesnow-fe/src/routes/index.js
@@ -1,7 +1,9 @@
 import routesConfig from "../config/routes";
+import Forbiden403 from "../pages/Forbiden403/Forbiden403";
 
 //import pages
 import Home from "../pages/Home";
+import NotFound404 from "../pages/NotFound404/NotFound404";
 
 //import layout
 
@@ -11,6 +13,8 @@ const publicRoutes = [];
 
 const privateRoutes = [
 	{ path: routesConfig.home, component: Home, title: "Overall" },
+	{ path: routesConfig.notfound404, component: NotFound404, title: "" },
+	{ path: routesConfig.forbiden403, component: Forbiden403, title: "" },
 ];
 
 export { publicRoutes, privateRoutes };
