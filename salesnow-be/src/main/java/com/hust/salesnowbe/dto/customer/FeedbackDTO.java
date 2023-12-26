46fce07d779a574c43e2dba6845aafb18072b890
kanowfy <nguyendung2002hl@gmail.com>
2023-12-26 20:27:30 +0700

    add feedback services

---
+++ salesnow-be/src/main/java/com/hust/salesnowbe/dto/customer/FeedbackDTO.java
@@ -0,0 +1,21 @@
+package com.hust.salesnowbe.dto.customer;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FeedbackDTO {
+    @Positive
+    private Integer customerId;
+
+    @NotNull
+    private int evaluate;
+    @NotNull
+    @NotEmpty
+    private String content;
+
+}
