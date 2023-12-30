000f1573f385d5ae631e67ee810bb919f4e1b4aa
kanowfy <nguyendung2002hl@gmail.com>
2023-12-30 10:21:09 +0700

    add customer API

---
+++ salesnow-be/src/main/java/com/hust/salesnowbe/dto/customer/CustomerSpendingDTO.java
@@ -0,0 +1,27 @@
+package com.hust.salesnowbe.dto.customer;
+
+import com.hust.salesnowbe.models.Customer;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+
+public class CustomerSpendingDTO {
+    @NotNull
+    private Customer customer;
+    @NotNull
+    private BigDecimal totalAmount;
+    @NotNull
+    private BigDecimal orderCount;
+
+    public CustomerSpendingDTO(Customer customer, BigDecimal totalAmount, BigDecimal orderCount) {
+        this.customer = customer;
+        this.totalAmount = totalAmount;
+        this.orderCount = orderCount;
+    }
+
+}
